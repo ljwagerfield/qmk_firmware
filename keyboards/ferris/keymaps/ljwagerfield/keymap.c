6b106b6f67063a51e46001a7f2b1a306754acf4d
Lawrence Wagerfield <lawrence@bytescale.com>
2025-09-26 23:02:21 +0100

    Optimize base layer for prose writing + fine tune flow tap timeouts.

--- keyboards/ferris/keymaps/ljwagerfield/keymap.c
+++ keyboards/ferris/keymaps/ljwagerfield/keymap.c
@@ -6,12 +6,12 @@ enum layers { _ALPHA, _NUM, _ARR, _SYM, _FN, _HYP1 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Alpha (Hands Down Prometheus)
     [_ALPHA] = LAYOUT_split_3x5_2(
-        KC_F      , KC_P      , KC_D      , KC_L      , KC_B      ,
-        KC_Q     , KC_U      , KC_O      , KC_Y      , KC_V      ,
+        KC_B      , KC_P      , KC_D      , KC_L      , KC_J      ,
+        KC_QUOT     , KC_U      , KC_O      , KC_Y      , KC_Q      ,
         LOPT_T(KC_S) , LCTL_T(KC_N) , LT(_ARR, KC_T) , LCMD_T(KC_H) , KC_K      ,
-        KC_TAB    , LCMD_T(KC_A) , LT(_NUM, KC_E) , LCTL_T(KC_I) , LOPT_T(KC_C) ,
-        KC_BSPC      , KC_W      , KC_G      , KC_M      , KC_J     ,
-        KC_Z  , KC_DOT    , KC_COMMA   , KC_DQUO      , KC_ENT    ,
+        KC_Z    , LCMD_T(KC_A) , LT(_NUM, KC_E) , LCTL_T(KC_I) , LOPT_T(KC_C) ,
+        KC_F      , KC_W      , KC_G      , KC_M      , KC_PERC     ,
+        KC_CIRC  , KC_ENT    , KC_BSPC   , KC_TAB      , KC_V    ,
         LT(_FN, KC_X)    , LSFT_T(KC_R)   , 
         LT(_SYM, KC_SPC)  , LT(_HYP1, KC_ESC)
     ),
@@ -27,21 +27,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_ARR] = LAYOUT_split_3x5_2(
         KC_NO   , KC_NO      , KC_NO      , KC_NO      , KC_NO   ,
-        KC_NO   , LCTL(KC_LEFT)      , KC_UP      , LCTL(KC_RIGHT)      , KC_NO   ,
+        KC_NO   , KC_LEFT      , KC_UP      , KC_RIGHT      , KC_NO   ,
         LCMD(KC_X), LCMD(KC_C), KC_NO, QK_LAYER_LOCK, KC_NO,
         LCMD(KC_LEFT) , LOPT(KC_LEFT) , KC_DOWN , LOPT(KC_RIGHT) , LCMD(KC_RIGHT)  ,
-        KC_TRNS   , KC_NO   , KC_NO     , LCMD(KC_V)  , KC_NO  ,
-        KC_NO  , KC_LEFT      , LCMD(KC_Z)      , KC_RIGHT      , KC_TRNS  ,
+        KC_NO   , KC_NO   , KC_NO     , LCMD(KC_V)  , KC_NO  ,
+        KC_NO  , KC_TRNS      , KC_TRNS      , KC_TRNS      , LCMD(KC_Z)  ,
         KC_TRNS  , KC_LSFT , // TODO: KC_LSFT must also be shift on hold
         KC_SPC     , KC_TRNS
     ),
     [_SYM] = LAYOUT_split_3x5_2(
-        KC_AT   , KC_AMPR      , KC_SLSH      , KC_QUOT      , KC_NO   ,
-        KC_NO      , KC_KP_ASTERISK, KC_GRAVE      , KC_PLUS      , KC_PIPE   ,
-        LOPT_T(KC_MINUS)   , LCTL_T(KC_LABK)   , KC_EQL     , LCMD_T(KC_RABK)   , KC_TILDE   ,
-        KC_HSH , LCMD_T(KC_RCBR) , KC_LCBR , LOPT_T(KC_DOLLAR) , LCTL_T(KC_EXLM)  ,
-        KC_QUES   , KC_UNDS   , KC_LBRC     , KC_RBRC  , KC_POUND  ,
-        KC_CIRC  , KC_SEMICOLON      , KC_NO      , KC_NO      , KC_PERC  ,
+        KC_EXLM         , KC_GRAVE, KC_DOLLAR      , KC_LCBR      , KC_RCBR   ,
+        KC_NO      , KC_AMPERSAND, KC_COLN            , KC_PIPE, KC_PLUS   ,
+        LOPT_T(KC_LPRN)   , LCTL_T(KC_RPRN)   , KC_DQUO     , LCMD_T(KC_COMMA)   , KC_LBRC   ,
+        KC_TILDE , LCMD_T(KC_RABK) , KC_EQL , LOPT_T(KC_LABK) , LCTL_T(KC_MINUS)  ,
+        KC_QUES   , KC_KP_ASTERISK   , KC_SLSH     , KC_DOT  , KC_RBRC  ,
+        KC_NO  , KC_SEMICOLON      , KC_NO      , KC_NO      , KC_UNDERSCORE  ,
         KC_BSLS  , LSFT(KC_R) ,
         KC_NO    , KC_NO
     ),
@@ -56,12 +56,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO   , QK_BOOT
     ),
     [_HYP1] = LAYOUT_split_3x5_2(
-        HYPR(KC_F) , HYPR(KC_P) ,  HYPR(KC_D) , HYPR(KC_L) , HYPR(KC_B) ,
-        HYPR(KC_Q) , HYPR(KC_U) , HYPR(KC_O) , HYPR(KC_Y) , HYPR(KC_V) ,
+        HYPR(KC_B) , HYPR(KC_P) ,  HYPR(KC_D) , HYPR(KC_L) , HYPR(KC_J) ,
+        KC_NO , HYPR(KC_U) , HYPR(KC_O) , HYPR(KC_Y) , HYPR(KC_Q) ,
         HYPR(KC_S) , HYPR(KC_N) , HYPR(KC_T) , HYPR(KC_H) , HYPR(KC_K) ,
-        HYPR(KC_TAB) , HYPR(KC_A) , HYPR(KC_E) , HYPR(KC_I) , HYPR(KC_C) ,
-        HYPR(KC_BSPC) , HYPR(KC_W) , HYPR(KC_G) , HYPR(KC_M) , HYPR(KC_J) ,
-        HYPR(KC_Z) , HYPR(KC_DOT) , HYPR(KC_COMMA) , HYPR(KC_QUOT) , HYPR(KC_ENT) ,
+        HYPR(KC_Z) , HYPR(KC_A) , HYPR(KC_E) , HYPR(KC_I) , HYPR(KC_C) ,
+        HYPR(KC_F) , HYPR(KC_W) , HYPR(KC_G) , HYPR(KC_M) , KC_NO ,
+        KC_NO , HYPR(KC_ENT) , HYPR(KC_BSPC) , HYPR(KC_TAB) , HYPR(KC_V) ,
         HYPR(KC_X) , HYPR(KC_R) ,
         KC_NO , KC_NO
     )
@@ -77,23 +77,23 @@ const key_override_t sh_spc_to_caps =
         MOD_MASK_CTRL | MOD_MASK_ALT | MOD_MASK_GUI   // but NOT if other mods are down
     );
 
-// Shift + .  ->  :
-const key_override_t sh_dot__to__colon =
-  ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_COLN);
+// Shift + %  ->  £
+const key_override_t sh_perc__to__pound =
+  ko_make_basic(MOD_MASK_SHIFT, KC_PERC, KC_POUND);
 
-// Shift + ,  ->  )
-const key_override_t sh_comm__to__rparen =
-  ko_make_basic(MOD_MASK_SHIFT, KC_COMM, KC_RPRN);
+// Shift + ^  ->  @
+const key_override_t sh_hat__to__at =
+  ko_make_basic(MOD_MASK_SHIFT, KC_CIRC, KC_AT);
 
-// Shift + "  ->  (
-const key_override_t sh_dquo__to__lparen =
-  ko_make_basic(MOD_MASK_SHIFT, KC_DQUO, KC_LPRN);
+// Shift + '  ->  #
+const key_override_t sh_quot__to__hash =
+  ko_make_basic(MOD_MASK_SHIFT, KC_QUOT, KC_HASH);
 
 const key_override_t *key_overrides[] = {
   &sh_spc_to_caps,
-  &sh_dot__to__colon,
-  &sh_comm__to__rparen,
-  &sh_dquo__to__lparen,
+  &sh_perc__to__pound,
+  &sh_hat__to__at,
+  &sh_quot__to__hash,
   NULL
 };
 
@@ -109,11 +109,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     switch (keycode) {
         case LT(_FN, KC_PERC): return tap(KC_PERC);
-        case LCMD_T(KC_RCBR): return tap(KC_RCBR);
-        case LCTL_T(KC_LABK): return tap(KC_LABK);
+        case LOPT_T(KC_LPRN): return tap(KC_LPRN);
+        case LCTL_T(KC_RPRN): return tap(KC_RPRN);
         case LCMD_T(KC_RABK): return tap(KC_RABK);
-        case LOPT_T(KC_DOLLAR): return tap(KC_DOLLAR);
-        case LCTL_T(KC_EXLM): return tap(KC_EXLM);
+        case LOPT_T(KC_LABK): return tap(KC_LABK);
+        case LCTL_T(KC_MINUS): return tap(KC_MINUS);
     }
 
     return true;
@@ -157,15 +157,28 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t* record,
 
     // Tap/holds where the tap needs to be part of a flow tap session, but we also want quick access to the layers exposed by the holds.
     switch (get_tap_keycode(keycode)) {
-        case KC_SPC:
-            return 0;
         case KC_T:
         case KC_E:
             // Increase this number if we accidentally trigger holds when typing fast.
             return 100; 
+        case KC_SPC:
+            if (get_tap_keycode(prev_keycode) == KC_SPC) {
+                return 0; // Optimisation: Allows symbols and shifted chars immediately after a space 
+            }
+
+            // Lower value is possible here as space is basically never an SFB or double-tapped (unlike R) so is faster,
+            // plus requires less thinking to type space, so generally seems faster / can get away with a shorter flow 
+            // tap timeout to allow the symbols layer to be more responsive.
+            return 50; 
         case KC_R:
-            // Shifting tends to happen quite fast, for example spacebar capital I.
-            return 0; 
+            if (get_tap_keycode(prev_keycode) == KC_SPC) {
+                return 0; // Optimisation: Allows symbols and shifted chars immediately after a space 
+            }
+
+            // Seems to be the lowest number to allow us to consistently type words like "terror" -- with 2 consecutive Rs which are slower -- within a flow tap.
+            // The lower the better (we still want "R" to be in flow tap), but if we get faster at typing and can make this
+            // lower, then it will result in fewer accidently "r" taps when we actually wanted "shift" holds instead.
+            return 90; 
     }
 
     return FLOW_TAP_TERM;
